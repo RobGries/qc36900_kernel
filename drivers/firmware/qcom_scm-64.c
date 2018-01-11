555b6d52c024927c079de156dfa701ef0257599d
Ramon Fried <rfried@codeaurora.org>
2018-01-11 03:02:00 +0200

    Revert "drivers: firmware: qcom: Enable download mode"
    This reverts commit 2734bd35927726dd94e97a63818df57f5b232feb.

--- drivers/firmware/qcom_scm-64.c
+++ drivers/firmware/qcom_scm-64.c
@@ -566,19 +566,6 @@ int __qcom_scm_iommu_secure_ptbl_init(struct device *dev, u64 addr, u32 size,
 	return ret;
 }
 
-int __qcom_scm_set_dload_mode(struct device *dev, bool enable)
-{
-	struct qcom_scm_desc desc = {0};
-	struct arm_smccc_res res;
-
-	desc.args[0] = QCOM_SCM_SET_DLOAD_MODE;
-	desc.args[1] = enable ? QCOM_SCM_SET_DLOAD_MODE : 0;
-	desc.arginfo = QCOM_SCM_ARGS(2);
-
-	return qcom_scm_call(dev, QCOM_SCM_SVC_BOOT, QCOM_SCM_SET_DLOAD_MODE,
-			     &desc, &res);
-}
-
 int __qcom_scm_iommu_dump_fault_regs(struct device *dev, u32 id, u32 context,
 				     u64 addr, u32 len)
 {
